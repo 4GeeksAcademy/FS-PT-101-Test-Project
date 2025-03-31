ccd3f6b64d591b38490c1e08ed4f110d1c3d33ab
JorgeArevalo-hub <jorgearevalomoran@gmail.com>
2025-03-31 11:02:11 +0000

    checkpoint: function import not working

---
+++ app.js
@@ -0,0 +1,11 @@
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+function fromEuroToDollar(euro){
+    let resultado = oneEuroIs.USD * euro;
+    return resultado
+    }
+
+    
---
+++ test.js
@@ -0,0 +1,13 @@
+test("One euro should be 1.07 dollars", function() {
+    const { fromEuroToDollar } = require('./app.js');
+    // Import the function from app.js
+
+    // Use the function like its supposed to be used
+    const dollars = fromEuroToDollar(3.5);
+
+    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
+    const expected = 3.5 * 1.07; 
+    
+    // This is the comparison for the unit test
+     expect(dollars).toBe(expected); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+})
