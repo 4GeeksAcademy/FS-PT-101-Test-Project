51476851f620a6e59ee7c611784d2b04bcaacd34
JorgeArevalo-hub <jorgearevalomoran@gmail.com>
2025-03-31 11:39:37 +0000

    feature: exercise completed

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ let oneEuroIs = {
     "GBP": 0.87, // british pound
 }
 function fromEuroToDollar(euro){
-    let resultado = oneEuroIs.USD * euro;
-    return resultado
+    let result = parseFloat(oneEuroIs.USD * euro).toFixed(2);
+    return result
     }
+    
+function fromDollarToYen(dollar){
+    let eur = dollar/oneEuroIs.USD;
+    let result = parseFloat(eur * oneEuroIs.JPY).toFixed(2);
+    return result
+    }
+
+function fromYenToPound(yen){
+    let eur = yen/oneEuroIs.JPY;
+    let result = parseFloat(eur * oneEuroIs.GBP).toFixed(2)
+    return result
+    }
+
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
 
     
--- test.js
+++ test.js
@@ -1,13 +1,42 @@
 test("One euro should be 1.07 dollars", function() {
-    const { fromEuroToDollar } = require('./app.js');
+
     // Import the function from app.js
+    const { fromEuroToDollar } = require('./app.js');
 
     // Use the function like its supposed to be used
     const dollars = fromEuroToDollar(3.5);
 
     // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
-    const expected = 3.5 * 1.07; 
+    const expected = parseFloat(3.5 * 1.07).toFixed(2); 
     
     // This is the comparison for the unit test
      expect(dollars).toBe(expected); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+})
+test("500 dollars should be 73130.84 yens", function() {
+
+    // Import the function from app.js
+    const { fromDollarToYen } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const yens = fromDollarToYen(500);
+
+    // If 1 euro is 1.07 dollars and 156.5 yens, then 500 dollars should be = ((500 / 1.07) * 156.5)
+    const expected = parseFloat((500 / 1.07) * 156.5).toFixed(2); 
+    
+    // This is the comparison for the unit test
+     expect(yens).toBe(expected); // 1 euro is 1.07 dollars and 156.5 yens, then 500 dollars should be = ((500 / 1.07) * 156.5)
+})
+test("170000 yens should be 945.04 pounds", function() {
+
+    // Import the function from app.js
+    const { fromYenToPound } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const pounds = fromYenToPound(170000);
+
+    // 1 euro is 0.87 pounds and 156.5 yens, then 1700000 yens should be = ((170000/156.5) * 0.87)
+    const expected = parseFloat((170000/156.5) * 0.87).toFixed(2); 
+    
+    // This is the comparison for the unit test
+     expect(pounds).toBe(expected); // 1 euro is 0.87 pounds and 156.5 yens, then 1700000 yens should be = ((170000/156.5) * 0.87)
 })
